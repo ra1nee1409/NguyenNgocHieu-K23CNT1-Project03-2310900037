0cd2731268480a646308433cdf502e753ba2e123
ra1nee1409 <sthieu14092005@gmail.com>
2025-10-21 17:34:47 +0700

    update bai 2

---
+++ K23CNT1-Nnh.Day02/src/main/java/K23CNT1_Nnh/Day02/K23CNT1_Nnh/Day02/Application.java
@@ -0,0 +1,14 @@
+package K23CNT1_Nnh.Day02.K23CNT1_Nnh.Day02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+        System.out.printf("Nguyen Ngoc Hieu - 2310900037 \n");
+	}
+
+}
---
+++ K23CNT1-Nnh.Day02/src/test/java/K23CNT1_Nnh/Day02/K23CNT1_Nnh/Day02/ApplicationTests.java
@@ -0,0 +1,13 @@
+package K23CNT1_Nnh.Day02.K23CNT1_Nnh.Day02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
